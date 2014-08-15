e5057170853dc90265b31e548afb56ca45234d00
Carlos Carvalho <ccarvalho@expertisesolutions.com.br>
2014-08-15 16:17:05 -0300

    Fixed (hopefully) basic compilation for C++

--- src/emc.cc
+++ src/emc.cc
@@ -1,10 +1,59 @@
+#ifdef HAVE_CONFIG_H
+# include <elementary_config.h>
+#endif
+
+#define ELM_INTERNAL_API_ARGESFSDFEFC
+#define ELM_INTERFACE_ATSPI_ACCESSIBLE_PROTECTED
+#define ELM_INTERFACE_ATSPI_COMPONENT_PROTECTED
+#define ELM_INTERFACE_ATSPI_ACTION_PROTECTED
+#define ELM_INTERFACE_ATSPI_VALUE_PROTECTED
+#define ELM_INTERFACE_ATSPI_EDITABLE_TEXT_PROTECTED
+#define ELM_INTERFACE_ATSPI_TEXT_PROTECTED
+#define ELM_INTERFACE_ATSPI_SELECTION_PROTECTED
+#define ELM_INTERFACE_ATSPI_IMAGE_PROTECTED
+#define ELM_INTERFACE_ATSPI_WIDGET_ACTION_PROTECTED
+
 #include <iostream>
 
-#include <Eina.h>
-#include <Ecore.h>
+#include <Eo.h>
+#include <Evas.h>
+#include <Elementary.h>
+#include <elm_widget.h>
+
+#include "elm_interface_atspi_accessible.h"
+#include "elm_interface_atspi_accessible.eo.h"
+
+#include "elm_interface_atspi_widget_action.h"
+#include "elm_interface_atspi_widget_action.eo.h"
+#include <elm_win.eo.hh>
+#include <elm_box.eo.hh>
+#include <elm_button.eo.hh>
+
+#include <Eina.hh>
+
+#include <deque>
+
 extern int emc_avplayer(void);
 
-int main(int argc, char **argv)
+#if 1
+struct clean_ref
+{
+  clean_ref(efl::eo::base base)
+    : _ref(base._eo_ptr())
+  {}
+
+  template <typename T>
+  void operator()(T const&, Eo_Event_Description const&, void*) const
+  {
+    if(_ref)
+      eo_unref(_ref);
+  }
+
+  Eo* _ref;
+};
+#endif
+EAPI_MAIN int
+elm_main(int argc, char **argv)
 {
    ecore_init();
 
@@ -14,3 +63,4 @@ int main(int argc, char **argv)
    ecore_shutdown();
    return 0;
 }
+ELM_MAIN()
